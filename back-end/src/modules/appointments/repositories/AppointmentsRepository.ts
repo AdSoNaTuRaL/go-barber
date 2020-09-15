479152db98d3a18ce81baa56cf6c76c180307118
adsonatural <adsonhenriquesilva@gmail.com>
2020-09-15 13:52:28 +0200

    Replacing folders and paths

--- back-end/src/modules/appointments/repositories/AppointmentsRepository.ts
+++ back-end/src/modules/appointments/repositories/AppointmentsRepository.ts
@@ -1,13 +1,13 @@
 import { EntityRepository, Repository } from 'typeorm';
 
-import Appointment from '../models/Appointment';
+import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
 
 @EntityRepository(Appointment)
 class AppointmentsRepository extends Repository<Appointment> {
     public async findByDate(date: Date): Promise<Appointment | null> {
         const findAppointment = await this.findOne({
             where: { date },
-        })
+        });
 
         return findAppointment || null;
     }
--- back-end/src/shared/infra/http/server.ts
+++ back-end/src/shared/infra/http/server.ts
@@ -1,13 +1,14 @@
 import 'reflect-metadata';
 
 import express, { Request, Response, NextFunction } from 'express';
-import 'express-async-errors';
 import cors from 'cors';
-import routes from './routes';
-import uploadConfig from './config/upload';
+import 'express-async-errors';
 
-import './database';
-import AppError from './errors/AppError';
+import uploadConfig from '@config/upload';
+import AppError from '@shared/errors/AppError';
+import routes from './routes';
+
+import '@shared/infra/typeorm';
 
 const app = express();
 
