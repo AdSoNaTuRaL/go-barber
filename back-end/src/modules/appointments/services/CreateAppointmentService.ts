54e81160fb71bbc5d2a55842e8151feeb5bc28c5
adsonatural <adsonhenriquesilva@gmail.com>
2020-09-15 17:12:29 +0200

    Refactoring

--- back-end/src/modules/appointments/services/CreateAppointmentService.ts
+++ back-end/src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,25 +1,30 @@
 import { startOfHour } from 'date-fns';
-import { getCustomRepository } from 'typeorm';
+import { inject, injectable } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
 
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
-import AppointmentsRepository from '@modules/appointments/repositories/AppointmentsRepository';
+import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
-interface Request {
+interface IRequest {
     provider_id: string;
     date: Date;
 }
 
+@injectable()
 class CreateAppointmentService {
-    public async execute({ date, provider_id }: Request): Promise<Appointment> {
-        const appointmentsRepository = getCustomRepository(
-            AppointmentsRepository,
-        );
+    constructor(
+        @inject('AppointmentsRepository')
+        private appointmentsRepository: IAppointmentsRepository,
+    ) {}
 
+    public async execute({
+        date,
+        provider_id,
+    }: IRequest): Promise<Appointment> {
         const appointmentDate = startOfHour(date);
 
-        const findAppointmentInSameDate = await appointmentsRepository.findByDate(
+        const findAppointmentInSameDate = await this.appointmentsRepository.findByDate(
             appointmentDate,
         );
 
@@ -27,12 +32,10 @@ class CreateAppointmentService {
             throw new AppError('This appointment is already booked!');
         }
 
-        const appointment = appointmentsRepository.create({
+        const appointment = await this.appointmentsRepository.create({
             provider_id,
             date: appointmentDate,
         });
-
-        await appointmentsRepository.save(appointment);
 
         return appointment;
     }
--- back-end/src/modules/users/infra/http/routes/users.routes.ts
+++ back-end/src/modules/users/infra/http/routes/users.routes.ts
@@ -1,49 +1,22 @@
 import { Router } from 'express';
 import multer from 'multer';
-import CreateUserService from '@modules/users/services/CreateUserService';
 import uploadConfig from '@config/upload';
-import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+import UsersController from '../controllers/UsersController';
+import UserAvatarController from '../controllers/UserAvatarController';
 
 const usersRouter = Router();
 const upload = multer(uploadConfig);
+const usersController = new UsersController();
+const userAvatarController = new UserAvatarController();
 
-usersRouter.post('/', async (request, response) => {
-    try {
-        const { name, email, password } = request.body;
-
-        const createUser = new CreateUserService();
-
-        const user = await createUser.execute({
-            name,
-            email,
-            password,
-        });
-
-        delete user.password;
-
-        return response.json(user);
-    } catch (err) {
-        return response.status(400).json({ error: err.message });
-    }
-});
+usersRouter.post('/', usersController.create);
 
 usersRouter.patch(
     '/avatar',
     ensureAuthenticated,
     upload.single('avatar'),
-    async (request, response) => {
-        const updateUserAvatar = new UpdateUserAvatarService();
-
-        const user = await updateUserAvatar.execute({
-            user_id: request.user.id,
-            avatarFilename: request.file.filename,
-        });
-
-        delete user.password;
-
-        return response.json({ user });
-    },
+    userAvatarController.update,
 );
 
 export default usersRouter;
