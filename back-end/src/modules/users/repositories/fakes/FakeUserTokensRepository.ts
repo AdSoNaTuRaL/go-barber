2aeba40988d183236c73c445f945c44c3866975d
adsonatural <adsonhenriquesilva@gmail.com>
2020-10-05 23:09:20 +0200

    Creating new services and tests

--- back-end/src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
+++ back-end/src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
@@ -13,9 +13,19 @@ class FakeUserTokensRepository implements IUserTokensRepository {
             id: v4(),
             token: v4(),
             user_id,
+            created_at: new Date(),
+            updated_at: new Date(),
         });
 
         this.userTokens.push(userToken);
+
+        return userToken;
+    }
+
+    public async findByToken(token: string): Promise<UserToken | undefined> {
+        const userToken = this.userTokens.find(
+            findToken => findToken.token === token,
+        );
 
         return userToken;
     }
