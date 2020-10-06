618c50fb4d5d0e93c888fe7a2d3bfd37c8401c78
adsonatural <adsonhenriquesilva@gmail.com>
2020-10-06 23:06:06 +0200

    Create new service and unitary test for profile information user

---
+++ back-end/src/modules/users/services/ShowProfileService.spec.ts
@@ -0,0 +1,37 @@
+import AppError from '@shared/errors/AppError';
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import ShowProfileService from './ShowProfileService';
+
+let fakeUsersRepository: FakeUsersRepository;
+let showProfile: ShowProfileService;
+
+describe('UpdateProfile', () => {
+    beforeEach(() => {
+        fakeUsersRepository = new FakeUsersRepository();
+
+        showProfile = new ShowProfileService(fakeUsersRepository);
+    });
+
+    it('should be able to show the profile information', async () => {
+        const user = await fakeUsersRepository.create({
+            name: 'John Doe',
+            email: 'johndoe@example.com',
+            password: '123456',
+        });
+
+        const profile = await showProfile.execute({
+            user_id: user.id,
+        });
+
+        expect(profile.name).toBe('John Doe');
+        expect(profile.email).toBe('johndoe@example.com');
+    });
+
+    it('should be not able to show the profile information from non-existing user', async () => {
+        await expect(
+            showProfile.execute({
+                user_id: 'non-existing-user-id',
+            }),
+        ).rejects.toBeInstanceOf(AppError);
+    });
+});
