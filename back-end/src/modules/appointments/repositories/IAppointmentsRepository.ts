d2f27f31dac301c206b5aecf8cfcadb7311ba5a8
adsonatural <adsonhenriquesilva@gmail.com>
2020-10-07 14:10:02 +0200

    Create interface and methods for provider availability (day and month availability)

--- back-end/src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ back-end/src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -1,7 +1,15 @@
 import ICreateAppointmentDTO from '../dtos/ICreateAppointmentDTO';
+import IFindAllInMonthFromProviderDTO from '../dtos/IFindAllInMonthFromProviderDTO';
+import IFindAllInDayFromProviderDTO from '../dtos/IFindAllInDayFromProviderDTO';
 import Appointment from '../infra/typeorm/entities/Appointment';
 
 export default interface IAppointmentsRepository {
     create(data: ICreateAppointmentDTO): Promise<Appointment>;
     findByDate(date: Date): Promise<Appointment | undefined>;
+    findAllInMonthFromProvider(
+        data: IFindAllInMonthFromProviderDTO,
+    ): Promise<Appointment[]>;
+    findAllInDayFromProvider(
+        data: IFindAllInDayFromProviderDTO,
+    ): Promise<Appointment[]>;
 }
