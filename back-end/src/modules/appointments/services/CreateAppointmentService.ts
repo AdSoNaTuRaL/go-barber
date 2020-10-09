e722c6a2d4ded9222a67cb5ab625e0a8ced1f572
adsonatural <adsonhenriquesilva@gmail.com>
2020-10-09 13:44:03 +0200

    Creating cache data for appointments

--- back-end/src/modules/appointments/services/CreateAppointmentService.ts
+++ back-end/src/modules/appointments/services/CreateAppointmentService.ts
@@ -3,6 +3,7 @@ import { inject, injectable } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
 
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
 import INotificationsRepository from '@modules/notifications/repositories/INotificationsRepository';
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
@@ -12,7 +13,6 @@ interface IRequest {
     user_id: string;
     date: Date;
 }
-
 @injectable()
 class CreateAppointmentService {
     constructor(
@@ -21,6 +21,9 @@ class CreateAppointmentService {
 
         @inject('NotificationsRepository')
         private notificationsRepository: INotificationsRepository,
+
+        @inject('CacheProvider')
+        private cacheProvider: ICacheProvider,
     ) {}
 
     public async execute({
@@ -69,6 +72,13 @@ class CreateAppointmentService {
             recipient_id: provider_id,
             content: `Novo agendamento para o dia ${formattedDate}`,
         });
+
+        await this.cacheProvider.invalidate(
+            `provider-appointments:${provider_id}:${format(
+                appointmentDate,
+                'yyyy-M-d',
+            )}`,
+        );
 
         return appointment;
     }
