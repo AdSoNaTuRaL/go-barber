19dd3b46b69449646134619d6e958b0f6a77b2ee
adsonatural <adsonhenriquesilva@gmail.com>
2020-10-09 13:43:29 +0200

    Deleting parseISO (date-fns) cause celebrate library is  already doing validation

--- back-end/src/modules/appointments/infra/http/controllers/AppointmentsController.ts
+++ back-end/src/modules/appointments/infra/http/controllers/AppointmentsController.ts
@@ -1,4 +1,3 @@
-import { parseISO } from 'date-fns';
 import { container } from 'tsyringe';
 import { Request, Response } from 'express';
 
@@ -13,14 +12,12 @@ export default class AppointmentsController {
 
         const { provider_id, date } = request.body;
 
-        const parsedDate = parseISO(date);
-
         const createAppointment = container.resolve(CreateAppointmentService);
 
         const appointment = await createAppointment.execute({
             provider_id,
             user_id,
-            date: parsedDate,
+            date,
         });
 
         return response.json({ appointment });
