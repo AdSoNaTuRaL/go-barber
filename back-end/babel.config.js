eff29fbabd3969c9ef88977bd4e4e67b4fa78775
adsonatural <adsonhenriquesilva@gmail.com>
2020-10-26 10:40:45 +0100

    Changing babel configurations

--- back-end/babel.config.js
+++ back-end/babel.config.js
@@ -10,6 +10,9 @@ module.exports = {
         "@config": "./src/config",
         "@shared": "./src/shared"
       }
-    }]
+    }],
+    "babel-plugin-transform-typescript-metadata",
+    ["@babel/plugin-proposal-decorators", { "legacy": true }],
+    ["@babel/plugin-proposal-class-properties", { "loose": true }],
   ],
 }
